392cd8cd30f4f2dfc426dffb207cf7fea244063f
shashivardhan3 <shashivardhan072@gmail.com>
2023-09-27 19:16:35 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from app.forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
     return render(request,'home.html')
 
 
@@ -22,9 +29,9 @@ def registration(request):
              MPFDO=PFDO.save(commit=False)
              MPFDO.username=MUFDO
              MPFDO.save()
-             send_mail('Registratioon',"Succefully Registration is Done",'shashivardhan072@gmail.com',
+             send_mail('Registration',"Succefully Registration is Done",'shashivardhan072@gmail.com',
                       [MUFDO.email],fail_silently=False)
-             return HttpResponse('Regsitration is Susssessfulll')
+             return HttpResponse('Regsitration is Successfulll')
         else:
             return HttpResponse('Not valid')
 
@@ -33,8 +40,24 @@ def registration(request):
     return render(request,'registration.html',d)
 
 
-    
-    
+def user_login(request):
+    if request.method=="POST":
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active():
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('login credentials are invalid')
+    return render(request,'user_login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
     
 
     
